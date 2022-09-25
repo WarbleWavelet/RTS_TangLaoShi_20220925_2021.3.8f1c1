60e7755d1b71164f19ff9057d68d204a00b5efc6
WWS <3027565221@qq.com>
2022-09-25 20:51:22 +0800

    单位预制体 动画器

---
+++ RTS_TangLaoShi_20220925_2021.3.8f1c1/Assets/ArtRes/Scene/Constants.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Constants_Animator
+{
+    public const string Key_IsMove = "IsMove"; 
+    public static bool Value_IsMove = false; 
+}
